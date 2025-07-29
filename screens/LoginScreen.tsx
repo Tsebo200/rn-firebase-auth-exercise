23e70244dd85c8805e5574d2d52371f6b162c701
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-07-29 19:21:36 +0200

    Feat(Navigation): Link To Register
    Link to Registration if user has no account

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,13 +1,20 @@
 import { TextInput, SafeAreaView, StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
+import { loginUser } from '../services/authService';
+import { useNavigation } from '@react-navigation/native';
+import RegisterScreen from './RegistrationScreen';
 
 const LoginScreen = () => {
+  const navigation = useNavigation<any>();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   //   TODO: Login Function
-  const login = () => {}
+  const login = () => {
+    loginUser(email, password); //Dont forget to add this to parse credentials into frontend
+
+  }
 
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
@@ -30,13 +37,16 @@ const LoginScreen = () => {
             />
 
         <TouchableOpacity style={styles.button} onPress={login}>
-            <Text style={styles.buttonText}>Login Button</Text>
+            <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
 
         {/* TODO: Add Register Navigation */}
 
+        <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+          <Text style={styles.linkText}>Don't have an account? Register</Text>
+        </TouchableOpacity>
+
       </View>  
-      
     </SafeAreaView>
   )
 }
@@ -55,16 +65,25 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: 'black',
         marginTop: 15,
+        // borderRadius: 8,
+            // borderColor: '#ddd',
         paddingHorizontal: 10,
     },
     button: {
         backgroundColor: "black",
         textAlign: 'center',
         padding: 10,
+        // borderRadius: 8,
         marginTop: 30
     },
     buttonText: {
         textAlign: 'center',
-        color: 'white'
+        color: 'white',
+        fontWeight: 'bold',
+    },
+    linkText: {
+      paddingTop: 20,
+      // color: 'b',
+
     }
 })
